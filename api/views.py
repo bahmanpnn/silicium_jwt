7e6c72635120a8ad7d9315a77a97e7522eec6761
bahmanpnn <bahmanpn@gmail.com>
2023-04-01 08:13:57 +0330

    add serializer-model-router-hyperlink for course and userserializer-permissions

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets,permissions
+from .models import Course
+from .serializers import CourseSerializer,UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class CourseView(viewsets.ModelViewSet):
+    queryset=Course.objects.all()
+    serializer_class=CourseSerializer
+
+class UserView(viewsets.ModelViewSet):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    permission_classes=(permissions.IsAdminUser,)
