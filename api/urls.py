d7d3d6e747f274cf52cb57091791f36df1064c9a
bahmanpnn <bahmanpn@gmail.com>
2023-04-01 21:03:56 +0330

    project done but i didnt testgit status

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path,include
 from rest_framework import routers
 from . import views
+from rest_framework_simplejwt import views as jwt_views
+
+
 
 router=routers.DefaultRouter()
 router.register('',views.CourseView)
@@ -8,5 +11,8 @@ router.register('user',views.UserView)
 
 
 urlpatterns = [
-    path('v1/',include(router.urls))
+    path('v1/',include(router.urls)),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
 ]
